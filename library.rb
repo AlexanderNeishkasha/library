6f9cb4b74202b3f1752c260c3fd56ac65f514cae
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-11-10 20:48:34 +0200

    Add store method to library

--- library.rb
+++ library.rb
@@ -1,20 +1,46 @@
+require './file_manager/file_manager_factory'
+require './entities/author'
+require './entities/book'
+require './entities/reader'
+require './entities/order'
+
 class Library
   def initialize(filename = 'library.json')
     @authors = []
     @books = []
     @readers = []
-    @file_manager = FileManager.get_instance(self, filename)
+    @orders = []
+    @file_manager = FileManagerFactory.create(self, filename)
+    @file_manager.read
+  end
+
+  def store
+    @file_manager.store
+  end
+
+  def to_hash
+    to_hash = Proc.new { |item| item.to_hash }
+    {
+        authors: @authors.map(&to_hash),
+        books: @books.map(&to_hash),
+        readers: @readers.map(&to_hash),
+        orders: @orders.map(&to_hash),
+    }
   end
 
   def add_author(author)
-    @authors.push(author) if (author.is_a? Author)
+    @authors.push(author) if author.is_a? Author
   end
 
   def add_book(book)
-    @books.push(book) if (book.is_a? Book)
+    @books.push(book) if book.is_a? Book
   end
 
   def add_reader(reader)
-    @readers.push(reader) if (reader.is_a? Reader)
+    @readers.push(reader) if reader.is_a? Reader
+  end
+
+  def add_order(order)
+    @orders.push(order) if order.is_a? Order
   end
 end
