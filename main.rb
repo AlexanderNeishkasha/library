fbdb0108abbb08adf11c473d49e2f104ddb5beba
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-11-10 20:49:09 +0200

    Add test items

--- main.rb
+++ main.rb
@@ -1,3 +1,15 @@
 require './library'
 
 library = Library.new
+uncle_bob = Author.new('Robert Cecil Martin', 'Robert C. Martin (“Uncle Bob”) has been a programmer
+since 1970. He is founder of Uncle Bob Consulting, LLC, and cofounder with his son Micah Martin of The Clean Coders LLC.
+Martin has published dozens of articles in various trade journals and is a regular speaker at international conferences
+and trade shows')
+clean_code = Book.new('The clean code', uncle_bob)
+me = Reader.new('Alex', 'neishkasha.alexandr@gmail.com', 'Dnipro', 'Polya Avenue', '119')
+order = Order.new(clean_code, me)
+library.add_author(uncle_bob)
+library.add_book(clean_code)
+library.add_reader(me)
+library.add_order(order)
+library.store
