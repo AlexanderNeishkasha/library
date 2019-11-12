f993ec542c70d2076c411ed5eae6b2d50e941995
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-11-12 23:49:24 +0200

    Fix generator

--- generator/generator.rb
+++ generator/generator.rb
@@ -6,25 +6,33 @@ class Generator
   end
 
   def fake_reader
-    @library.add_reader(Reader.new(
+    reader = Reader.new(
       name: FFaker::Name.name,
       email: FFaker::Internet.email,
       city: FFaker::Address.city,
       street: FFaker::Address.street_name,
       house: FFaker::Address.building_number
-    ))
+    )
+    @library.add_reader(reader)
+    reader
   end
 
   def fake_author
-    @library.add_author(Author.new(FFaker::Name.name, FFaker::Lorem.paragraph))
+    author = Author.new(FFaker::Name.name, FFaker::Lorem.paragraph)
+    @library.add_author(author)
+    author
   end
 
   def fake_book
     author = fake_author
-    @library.add_book(Book.new(FFaker::Lorem.words(rand(3..10)), author))
+    book = Book.new(FFaker::Lorem.words(rand(3..10)), author)
+    @library.add_book(book)
+    book
   end
 
   def fake_order
-    @library.add_order(Order.new(fake_book, fake_reader, FFaker::Time.date))
+    order = Order.new(fake_book, fake_reader, FFaker::Time.date)
+    @library.add_order(order)
+    order
   end
 end
