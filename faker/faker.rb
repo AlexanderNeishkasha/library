7b646e39903b71326237162f94ea8340de7bf623
Alexander Neishkasha <alexander.ne@kultprosvet.net>
2019-11-16 23:22:11 +0200

    Refactoring for faker

---
+++ faker/faker.rb
@@ -0,0 +1,61 @@
+require 'ffaker'
+
+class Generator
+  def initialize(library)
+    @library = library
+  end
+
+  def fake_reader
+    Reader.new(
+      name: FFaker::Name.name,
+      email: FFaker::Internet.email,
+      city: FFaker::Address.city,
+      street: FFaker::Address.street_name,
+      house: rand(1..1000)
+    )
+  end
+
+  def fake_author
+    Author.new(FFaker::Name.name, FFaker::Lorem.paragraph)
+  end
+
+  def fake_book(author)
+    Book.new(FFaker::Lorem.sentence(rand(2..10)), author)
+  end
+
+  def fake_order(book, reader)
+    Order.new(book, reader, FFaker::Time.date)
+  end
+
+  def generate_fake_entities(count_readers, count_books)
+    authors = generate_fake_authors(count_books)
+    readers = generate_fake_readers(count_readers)
+    books = generate_fake_books(authors)
+    orders = generate_fake_orders(readers, books)
+    authors + books + readers + orders
+  end
+
+  def generate_fake_authors(count)
+    authors = []
+    count.times { authors.push(fake_author) }
+    authors
+  end
+
+  def generate_fake_readers(count)
+    readers = []
+    count.times { readers.push(fake_reader) }
+    readers
+  end
+
+  def generate_fake_books(authors)
+    books = []
+    authors.each { |author| books.push(fake_book(author)) }
+    books
+  end
+
+  def generate_fake_orders(readers, books)
+    orders = []
+    readers.each { |reader| books.each { |book| orders.push(fake_order(book, reader)) } }
+    orders
+  end
+end
--- main.rb
+++ main.rb
@@ -4,13 +4,16 @@ file_encoder = YamlEncoder.new('library.yml')
 library = file_encoder.read
 generator = Generator.new library
 
-rand(1..3).times do
-  generator.fake_order
-end
+generator.generate_fake_entities(2, 2).each { |entity| library.add_entity entity }
+generator.generate_fake_entities(1, 3).each { |entity| library.add_entity entity }
+generator.generate_fake_entities(3, 1).each { |entity| library.add_entity entity }
 
 statistics = Statistic.new library
+puts 'Top readers:'
 puts statistics.top_readers
+puts 'Most popular books:'
 puts statistics.most_populars_books
-puts statistics.count_readers_of_popular_books
+popular_books = 3
+puts "Count readers of #{popular_books} popular books: #{statistics.count_readers_of_popular_books(popular_books)}"
 
 file_encoder.store library
